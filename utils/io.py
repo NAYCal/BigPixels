7655fc1da62d34bf4bfeece1c20be91e3f418379
Alexander <naycal@berkeley.edu>
2023-12-18 22:35:29 -0800

    Added some io functionalities

--- utils/io.py
+++ utils/io.py
@@ -1,2 +1,64 @@
+import os
 import torch
+import warnings
+import numpy as np
+from PIL import Image
+from torchvision import transforms
 
+
+def get_image(file_name, directory_path="data", data_format="torch", out_size=None):
+    assert file_name.lower().endswith(
+        (".jpg", ".jpeg", ".png", ".bmp")
+    ), "File name must end with '.jpg', '.jpeg', '.png', '.bmp'"
+    assert data_format.lower() in {"torch", "numpy", "pil"}
+
+    image_path = os.path.join(directory_path, file_name)
+    image = Image.open(image_path)
+
+    if out_size is not None:
+        image = image.resize(out_size)
+
+    match data_format.lower():
+        case "torch":
+            converted_image = transforms.ToTensor()(image)
+        case "numpy":
+            converted_image = normalize_image(np.array(image).astype(np.float32))
+        case "pil":
+            converted_image = image
+
+    return converted_image
+
+
+def save_image(img_rep, file_name, directory_path="out", out_size=None):
+    assert file_name.lower().endswith(
+        (".jpg", ".jpeg", ".png", ".bmp")
+    ), "File name must end with '.jpg', '.jpeg', '.png', '.bmp'"
+    os.makedirs(directory_path, exist_ok=True)
+    save_path = os.path.join(directory_path, file_name)
+
+    match type(img_rep):
+        case np.ndarray:
+            uarray = img_rep * 255 if img_rep.max() <= 1.0 else img_rep
+            uarray = uarray.astype(np.uint8)
+            image = Image.fromarray(uarray)
+        case torch.Tensor:
+            uarray = img_rep * 255 if img_rep.max() <= 1.0 else img_rep
+            uarray = uarray.numpy().transpose(1, 2, 0).astype(np.uint8)
+            image = Image.fromarray(uarray)
+        case _:
+            image = img_rep
+
+    if out_size is not None:
+        image = image.resize(out_size)
+    image.save(save_path)
+
+
+def normalize_image(image):
+    if not isinstance(image, np.ndarray) and not isinstance(image, torch.Tensor):
+        warnings.warn("WARNING: Image is not a ndarray or tensor")
+        return image
+
+    min_val = image.min()
+    max_val = image.max()
+
+    return (image - min_val) / (max_val - min_val)
