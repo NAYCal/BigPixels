deb73fb84775a740c9b03ef13c5fbd60bda701e5
Alexander <naycal@berkeley.edu>
2023-12-27 20:34:30 -0800

    Added samples and enhanced image frequencies

--- utils/image_frequencies.py
+++ utils/image_frequencies.py
@@ -12,13 +12,13 @@ def finite_difference(
     reduce_noise=True,
     out_grayscale=True,
     threshold=None,
-    ksize=3,
-    ksigma=2,
+    ksize=30,
+    ksigma=30,
     device="cpu",
 ):
     match type(image):
         case np.ndarray:
-            numpy_finite_difference(
+            return numpy_finite_difference(
                 image,
                 reduce_noise=reduce_noise,
                 out_grayscale=out_grayscale,
@@ -27,7 +27,7 @@ def finite_difference(
                 threshold=threshold,
             )
         case torch.Tensor:
-            tensor_finite_difference(
+            return tensor_finite_difference(
                 image,
                 reduce_noise=reduce_noise,
                 out_grayscale=out_grayscale,
@@ -41,7 +41,7 @@ def finite_difference(
 
 
 def numpy_finite_difference(
-    image, reduce_noise=True, out_grayscale=True, ksize=3, ksigma=2, threshold=None
+    image, reduce_noise=True, out_grayscale=True, ksize=30, ksigma=30, threshold=None
 ):
     cov = lambda img, kernel: convolve2d(
         img, kernel, mode="same", boundary="fill", fillvalue=0
@@ -75,10 +75,15 @@ def numpy_finite_difference(
                 (derived > threshold).astype(np.float32) if threshold else derived
             )
         case 4:
-            diff_images = [
-                numpy_finite_difference(img, reduce_noise, out_grayscale, threshold)
-                for img in image
-            ]
+            diff_images = np.stack(
+                [
+                    numpy_finite_difference(
+                        image[i], reduce_noise, out_grayscale, threshold
+                    )
+                    for i in image.shape[0]
+                ],
+                axis=0,
+            )
             return diff_images
         case _:
             raise ValueError("Image must be a 2D object!")
@@ -110,7 +115,6 @@ def tensor_finite_difference(
     channel_dim = None
     match len(image.shape):
         case 2:
-            height, width = image.shape
             image = image.unsqueeze(0).unsqueeze(0)
         case 3:
             channel_dim = 0
@@ -118,7 +122,6 @@ def tensor_finite_difference(
             image = image.reshape(-1, 1, height, width)
         case 4:
             channel_dim = 1
-            height, width = image.shape[2:]
         case _:
             raise ValueError("Image must be a 2D object!")
 
@@ -134,14 +137,136 @@ def tensor_finite_difference(
         (derived > threshold).to(torch.float32) if threshold else derived
     )
 
-def image_sharpening(image, ksize=3, ksigma=2):
+
+def image_blurr(image, ksize=3, ksigma=2, device="cpu"):
     match type(image):
         case np.ndarray:
-            gaussian_kernel = numpy_gaussian_kernel(ksize=ksize, ksigma=ksigma)
+            return numpy_image_blurr(image=image, ksize=ksize, ksigma=ksigma)
         case torch.Tensor:
-            gaussian_kernel = tensor_gaussian_kernel(ksize=ksize, ksigma=ksigma)
+            return tensor_image_blurr(
+                image=image, ksize=ksize, ksigma=ksigma, device=device
+            )
         case _:
             raise TypeError("Image type not supported!")
 
-    low_freq = convolve2d(image, gaussian_kernel, mode="same", boundary="fill", fillvalue=0)
-    return normalize_image(image - low_freq)
+
+def numpy_image_blurr(image, ksize=3, ksigma=2):
+    kernel = numpy_gaussian_kernel(ksize=ksize, ksigma=ksigma)
+
+    cov = lambda img: convolve2d(img, kernel, mode="same", boundary="fill", fillvalue=0)
+
+    match len(image.shape):
+        case 2:
+            return normalize_image(cov(image))
+        case 3:
+            channels = []
+            for d in range(3):
+                channels.append(image[:, :, d])
+            return normalize_image(np.stack(channels, axis=-1))
+        case 4:
+            diff_images = np.stack(
+                [
+                    numpy_image_blurr(image[i], ksize=ksize, ksigma=ksigma)
+                    for i in range(image.shape[0])
+                ],
+                axis=0,
+            )
+            return diff_images
+        case _:
+            raise ValueError("Image must be a 2D object!")
+
+
+def tensor_image_blurr(image, ksize=3, ksigma=2, device="cpu"):
+    image = image.to(device)
+    original_shape = image.shape
+
+    kernel = (
+        tensor_gaussian_kernel(ksize=ksize, ksigma=ksigma)
+        .unsqueeze(0)
+        .unsqueeze(0)
+        .to(device=device)
+    )
+
+    match len(image.shape):
+        case 2:
+            image = image.unsqueeze(0).unsqueeze(0)
+        case 3:
+            height, width = image.shape[1:]
+            image = image.reshape(-1, 1, height, width)
+        case 4:
+            height, width = image.shape[2:]
+            image = image.reshape(-1, 1, height, width)
+        case _:
+            raise ValueError("Image must be a 2D object!")
+
+    return normalize_image(conv2d(image, kernel, padding="same").view(original_shape))
+
+
+def image_sharpen(image, alpha=0.5, ksize=3, ksigma=2, device="cpu"):
+    match type(image):
+        case np.ndarray:
+            return numpy_image_sharpen(image, alpha=alpha, ksize=ksize, ksigma=ksigma)
+        case torch.Tensor:
+            return tensor_image_sharpen(
+                image, alpha=alpha, ksize=ksize, ksigma=ksigma, device=device
+            )
+        case _:
+            raise TypeError("Image type not supported!")
+
+
+def numpy_image_sharpen(image, alpha=0.5, ksize=3, ksigma=2):
+    gaussian_kernel = numpy_gaussian_kernel(ksize=ksize, ksigma=ksigma)
+    center_y, center_x = gaussian_kernel.shape[0] // 2, gaussian_kernel.shape[1] // 2
+    unit_impulse = np.zeros_like(gaussian_kernel)
+
+    unit_impulse[center_y, center_x] = 1
+    sharpen_kernel = (1 + alpha) * unit_impulse - alpha * gaussian_kernel
+
+    cov = lambda img: convolve2d(
+        img, sharpen_kernel, mode="same", boundary="fill", fillvalue=0
+    )
+
+    match len(image.shape):
+        case 2:
+            return cov(image)
+        case 3:
+            return np.stack(
+                [cov(image[:, :, d]) for d in range(image.shape[2])], axis=-1
+            )
+        case 4:
+            return np.stack(
+                [
+                    [cov(image[i, :, :, d]) for d in range(image.shape[3])]
+                    for i in range(image.shape[0])
+                ],
+                axis=0,
+            )
+        case _:
+            raise ValueError("Image must be a 2D object!")
+
+
+def tensor_image_sharpen(image, alpha=0.5, ksize=3, ksigma=2, device="cpu"):
+    image = image.to(device)
+    original_shape = image.shape
+
+    gaussian_kernel = tensor_gaussian_kernel(ksize=ksize, ksigma=ksigma)
+    center_y, center_x = gaussian_kernel.shape[0] // 2, gaussian_kernel.shape[1] // 2
+    unit_impulse = torch.zeros_like(gaussian_kernel)
+
+    unit_impulse[center_y, center_x] = 1
+    sharpen_kernel = ((1 + alpha) * unit_impulse - alpha * gaussian_kernel).to(device)
+    sharpen_kernel = sharpen_kernel.unsqueeze(0).unsqueeze(0).to(device=device)
+
+    match len(image.shape):
+        case 2:
+            image = image.unsqueeze(0).unsqueeze(0)
+        case 3:
+            height, width = image.shape[1:]
+            image = image.reshape(-1, 1, height, width)
+        case 4:
+            height, width = image.shape[2:]
+            image = image.reshape(-1, 1, height, width)
+        case _:
+            raise ValueError("Image must be a 2D object!")
+
+    return conv2d(image, sharpen_kernel, padding="same").view(original_shape)
--- utils/image_io.py
+++ utils/image_io.py
@@ -7,18 +7,32 @@ from torchvision import transforms
 
 IMAGE_FILE_TYPES = (".jpg", ".jpeg", ".png", ".bmp", ".tif")
 
-def read_image(file_name, directory_path="data", data_format="torch", out_size=None, grayscale=False):
+
+def read_image(
+    file_name,
+    directory_path="data",
+    data_format="torch",
+    out_size=None,
+    grayscale=False,
+):
     assert file_name.lower().endswith(
         IMAGE_FILE_TYPES
     ), f"File name must end with one of the following: {IMAGE_FILE_TYPES}"
-    assert data_format.lower() in {"tensor", "torch", "numpy", "np", "pil", "pillow"}, "Data format must be valid!"
+    assert data_format.lower() in {
+        "tensor",
+        "torch",
+        "numpy",
+        "np",
+        "pil",
+        "pillow",
+    }, "Data format must be valid!"
 
     parent_directory = os.path.dirname(os.getcwd())
     image_path = os.path.join(parent_directory, directory_path, file_name)
     image = adjust_pillow_image(Image.open(image_path))
-    
+
     if grayscale:
-        image = image.convert('L')
+        image = image.convert("L")
 
     if out_size is not None:
         image = image.resize(out_size)
@@ -34,7 +48,12 @@ def read_image(file_name, directory_path="data", data_format="torch", out_size=N
     return converted_image
 
 
-def read_all_images(directory_path="data", data_format="torch", out_size=None):
+def read_all_images(
+    directory_path="data", data_format="torch", out_size=None, grayscale=False
+):
+    parent_directory = os.path.dirname(os.getcwd())
+    directory_path = os.path.join(parent_directory, directory_path)
+    
     image_paths = []
     file_names = []
     for root, _, files in os.walk(directory_path):
@@ -43,11 +62,25 @@ def read_all_images(directory_path="data", data_format="torch", out_size=None):
                 image_path = os.path.join(root, file)
                 image_paths.append(image_path)
                 file_names.append(file)
-
+    
     images = []
     for image_path in image_paths:
-        images.append(read_image(image_path, "", data_format=data_format, out_size=out_size))
+        images.append(
+            read_image(
+                image_path,
+                "",
+                data_format=data_format,
+                out_size=out_size,
+                grayscale=grayscale,
+            )
+        )
 
+    match data_format:
+        case "torch" | "tensor":
+            images = torch.stack(images, dim=0)
+        case "numpy" | "np":
+            images = np.stack(images, axis=0)
+    
     return images, file_names
 
 
@@ -85,6 +118,7 @@ def normalize_image(image):
 
     return (image - min_val) / (max_val - min_val)
 
+
 def adjust_pillow_image(image):
     exif = image._getexif()
     if exif:
@@ -92,7 +126,7 @@ def adjust_pillow_image(image):
         if orientation_tag in exif:
             orientation = exif[orientation_tag]
 
-            if orientation == 3: 
+            if orientation == 3:
                 image = image.rotate(180)
             elif orientation == 6:
                 image = image.rotate(-90, expand=True)
